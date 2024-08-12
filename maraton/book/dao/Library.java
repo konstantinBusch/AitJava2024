079043e5a0ffae316e1dbef43934b2b6a8c9ff8f
Konstantin <konstantin.busch@web.de>
2024-08-12 19:54:50 +0200

    добавил Library

---
+++ maraton/book/dao/Library.java
@@ -0,0 +1,12 @@
+package maraton.book.dao.maraton.book.dao;
+
+import maraton.book.book.Book;
+
+public interface Library  {
+    boolean addBook (Book book);//добавить книгу;
+    Book findBook(int isbn);//найти книгу;
+    Book deletBook (int isbn);//удалить книгу;
+    int quantity();//кол-во книг;
+    void printBook();//напечатать список книг.
+    Book[] findAuthor(String author);
+}
